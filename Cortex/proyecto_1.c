098e3c5dc6ad1dd80ed32be8488fc62386dd61d8
Simón Fallas <47420204+SimonFV@users.noreply.github.com>
2023-10-02 02:34:14 -0600

    Arreglado programa atascado.

--- Cortex/proyecto_1.c
+++ Cortex/proyecto_1.c
@@ -11,6 +11,8 @@
 #define HW_REGS_MASK (HW_REGS_SPAN - 1)
 #define SDRAM_BASE 0x08000000
 
+static int started = 0;
+
 int assign_mem(void **virtual_base, int *dev_mem, volatile unsigned short **sdram_addr)
 {
     // Open /dev/mem
@@ -60,7 +62,6 @@ int load_file(volatile unsigned short **sdram_addr)
 
     int number;
     int count = 0;
-
     while (fscanf(file, "%d", &number) == 1)
     {
         (*sdram_addr)[count] = (unsigned short)number;
@@ -71,18 +72,33 @@ int load_file(volatile unsigned short **sdram_addr)
     return 0;
 }
 
+/*
+void start(volatile unsigned short **sdram_addr, int offset)
+{
+    printf("Started...");
+    printf("Percentage Nios: %us, Cortex: %us", (*sdram_addr)[offset], 100 - (*sdram_addr)[offset]);
+
+    printf("Procesando...");
+
+    printf("Finalizado.");
+    (*sdram_addr)[offset] = (unsigned short)2;
+}*/
+
 int main()
 {
+    printf("asd2");
     void *virtual_base;
     int dev_mem = 0;
     volatile unsigned short *sdram_addr = NULL;
 
+    printf("asd2");
     if (assign_mem(&virtual_base, &dev_mem, &sdram_addr) != 0)
     {
         printf("Error during memory assignment.\n");
         return 1;
     }
 
+    printf("asd2");
     if (load_file(&sdram_addr))
     {
         printf("Error while loading the file.\n");
@@ -90,32 +106,20 @@ int main()
     }
 
     int size = sdram_addr[0] * sdram_addr[1];
-    int info_offset = size * 2 + 2;
-    int started = 0;
+    // int info_offset = size * 2 + 2;
+    // unsigned short start_flag = 0;
 
-    sdram_addr[info_offset + 1] = 0;
+    // sdram_addr[info_offset + 1] = 0;
 
     while (1)
     {
-        if (sdram_addr[info_offset + 1] == 1) // Start
-        {
-            if (started == 0)
-            {
-                started = 1;
-                printf("Started...");
-                printf("Percentage Nios: %us, Cortex: %us", sdram_addr[info_offset], 100 - sdram_addr[info_offset]);
-            }
-
-            // PROCESAR SEGUN PORCENTAJE
-
-            // continue;
-        }
-        else
-        {
-            started = 0;
-        }
-
-        usleep(1000);
+        printf("Cifrada: %u, %u, %u, %u\n", sdram_addr[0], sdram_addr[1], sdram_addr[2], sdram_addr[3]);
+        printf("Cifrada: %u, %u, %u, %u\n", sdram_addr[size - 2], sdram_addr[size - 1], sdram_addr[size], sdram_addr[size + 1]);
+        printf("Desc: %u, %u, %u, %u\n", sdram_addr[size + 2], sdram_addr[size + 3], sdram_addr[size + 4], sdram_addr[size + 5]);
+        printf("Desc: %u, %u, %u, %u\n", sdram_addr[size * 2 - 2], sdram_addr[size * 2 - 1], sdram_addr[size * 2], sdram_addr[size * 2 + 1]);
+        printf("Porcentaje: %u, \n", sdram_addr[size * 2 + 2]);
+        printf("Flag: %u\n", sdram_addr[size * 2 + 3]);
+        sleep(1);
     }
 
     clear_mem(&virtual_base, &dev_mem);
